5b79019d9ed776e7f3511e1cf1623675d3b61733
aznshrllh <ahmadzakinashirullah@gmail.com>
2025-01-31 10:00:15 +0700

    add types for rewards, privileges, and locations; create Navbar component

---
+++ clone-ui-bakmi-gm/src/Types.ts
@@ -0,0 +1,17 @@
+export type TReedemReward = {
+  name: string;
+  imgUrl: string;
+  point: number;
+};
+
+export type TPrivillages = {
+  name: string;
+  imgUrl: string;
+  expire: string;
+};
+
+export type TLocation = {
+  name: string;
+  address: string;
+  distance: number;
+};
