2b33bf875b8bbd547a756dfd71d61d86d0cecfc9
aznshrllh <ahmadzakinashirullah@gmail.com>
2025-01-31 15:59:12 +0700

    enhance Home and Banner components with new layout and dynamic content; add icon menu and improve carousel functionality

--- clone-ui-bakmi-gm/src/app/page.tsx
+++ clone-ui-bakmi-gm/src/app/page.tsx
@@ -118,25 +118,160 @@ export default function Home() {
       <div>
         <h1> Bakmi GM Points</h1>
       </div>
-      <div>
-        <h2>Icon Table</h2>
-      </div>
-      <div>
-        <h1>Image like banner</h1>
-      </div>
-      <div>
-        <div className="flex ">
-          <div className="w-4/5">
-            <h1 className="">Redeem Your Award</h1>
-            <h5>Earn more to get your rewards</h5>
+
+      {/* icon menu */}
+      <div className="px-4 py-2">
+        <div className="grid grid-cols-3 gap-4">
+          <div className="flex flex-col items-center">
+            <div className="w-12 h-12 bg-primary/10 rounded-full flex items-center justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+              </svg>
+            </div>
+            <span className="text-xs mt-1">Order</span>
           </div>
-          <div className="w-1/5">
-            <button className="btn btn-ghost">See all</button>
+
+          <div className="flex flex-col items-center">
+            <div className="w-12 h-12 bg-primary/10 rounded-full flex items-center justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </div>
+            <span className="text-xs mt-1">Redeem</span>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <div className="w-12 h-12 bg-primary/10 rounded-full flex items-center justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z"
+                />
+              </svg>
+            </div>
+            <span className="text-xs mt-1">My Voucher</span>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <div className="w-12 h-12 bg-primary/10 rounded-full flex items-center justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
+                />
+              </svg>
+            </div>
+            <span className="text-xs mt-1">Privilege</span>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <div className="w-12 h-12 bg-primary/10 rounded-full flex items-center justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </div>
+            <span className="text-xs mt-1">History</span>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <div className="w-12 h-12 bg-primary/10 rounded-full flex items-center justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+                />
+              </svg>
+            </div>
+            <span className="text-xs mt-1">Contact Us</span>
           </div>
         </div>
-        <div className="flex overflow-x-auto gap-4 px-4 py-2 snap-x scrollbar-hide">
+      </div>
+
+      {/* banner invite teman */}
+      <div className="px-4 py-2">
+        <div className="w-full h-40 rounded-lg overflow-hidden">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
+            alt="Promo Banner"
+            className="w-full h-full object-cover"
+          />
+        </div>
+      </div>
+
+      {/* redeem award */}
+      <div className="px-4 py-2">
+        <div className="flex justify-between items-center mb-2">
+          <div>
+            <h1 className="text-lg font-bold">Redeem Your Award</h1>
+            <h5 className="text-xs text-gray-500">
+              Earn more to get your rewards
+            </h5>
+          </div>
+          <button className="btn btn-ghost btn-sm">See all</button>
+        </div>
+        <div className="flex overflow-x-auto gap-3 snap-x scrollbar-hide -mx-4 px-4 -ml-1">
           {reedemRewards.map((reward) => (
-            <div className="flex-none w-[280px] snap-start" key={reward.id}>
+            <div
+              className="flex-none w-[160px] snap-start mb-2"
+              key={reward.id}
+            >
               <CardRedeemAward
                 name={reward.name}
                 imgUrl={reward.imgUrl}
@@ -146,10 +281,20 @@ export default function Home() {
           ))}
         </div>
       </div>
+
+      {/* privileges */}
       <div className="px-4">
         {" "}
         {/* Add padding for mobile */}
-        <h1 className="text-xl font-bold mb-4">Privillage</h1>
+        <div className="flex justify-between items-center mb-2">
+          <div>
+            <h1 className="text-lg font-bold">Privileges</h1>
+            <h5 className="text-xs text-gray-500">
+              Keep discovering our promo and news
+            </h5>
+          </div>
+        </div>
+        {/* <h1 className="text-xl font-bold mb-4">Privillage</h1> */}
         <div className="grid grid-cols-2 gap-2 auto-rows-auto">
           {privillages.map((privillage) => (
             <CardPrivillages
@@ -161,6 +306,8 @@ export default function Home() {
           ))}
         </div>
       </div>
+
+      {/* Locations */}
       <div className="px-4">
         <div className="space-y-4">
           <div className="flex flex-col mt-4">
--- clone-ui-bakmi-gm/src/components/Banner.tsx
+++ clone-ui-bakmi-gm/src/components/Banner.tsx
@@ -1,59 +1,43 @@
 export default function Banner() {
   return (
-    <div className="carousel w-full">
-      <div id="slide1" className="carousel-item relative w-full">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-          className="w-full"
-        />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide4" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide2" className="btn btn-circle">
-            ❯
-          </a>
+    <div className="relative">
+      <div className="carousel w-full scroll-smooth snap-x snap-mandatory">
+        <div id="item1" className="carousel-item w-full h-48">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
+            className="w-full h-full object-cover"
+          />
         </div>
-      </div>
-      <div id="slide2" className="carousel-item relative w-full">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-          className="w-full"
-        />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide1" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide3" className="btn btn-circle">
-            ❯
-          </a>
+        <div id="item2" className="carousel-item w-full h-48">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
+            className="w-full h-full object-cover"
+          />
         </div>
-      </div>
-      <div id="slide3" className="carousel-item relative w-full">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-          className="w-full"
-        />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide2" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide4" className="btn btn-circle">
-            ❯
-          </a>
+        <div id="item3" className="carousel-item w-full h-48">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
+            className="w-full h-full object-cover"
+          />
         </div>
-      </div>
-      <div id="slide4" className="carousel-item relative w-full">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-          className="w-full"
-        />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide3" className="btn btn-circle">
-            ❮
+        <div id="item4" className="carousel-item w-full h-48">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div className="absolute bottom-4 left-0 right-0 flex justify-center gap-2 z-10">
+          <a href="#item1">
+            <div className="w-2 h-2 bg-white/70 rounded-full shadow-md hover:bg-white data-[active=true]:bg-white"></div>
           </a>
-          <a href="#slide1" className="btn btn-circle">
-            ❯
+          <a href="#item2">
+            <div className="w-2 h-2 bg-white/70 rounded-full shadow-md hover:bg-white data-[active=true]:bg-white"></div>
+          </a>
+          <a href="#item3">
+            <div className="w-2 h-2 bg-white/70 rounded-full shadow-md hover:bg-white data-[active=true]:bg-white"></div>
+          </a>
+          <a href="#item4">
+            <div className="w-2 h-2 bg-white/70 rounded-full shadow-md hover:bg-white data-[active=true]:bg-white"></div>
           </a>
         </div>
       </div>
